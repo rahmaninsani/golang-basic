2608a71e979a46d712f0accce23ed53640e12dde
Rahman Insani <rahmaninsani54@gmail.com>
2022-08-17 20:23:56 +0700

    Learn: Operator

--- 9operator.go
+++ 9operator.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var result = 10 + 10
+	fmt.Println(result)
+
+	var a = 10
+	var b = 2
+	c := a * b
+	fmt.Println(c)
+
+	var d = 10
+	d += 10
+	fmt.Println(d)
+
+	var f = 19
+	f++
+	fmt.Println(f)
+
+	e := 21
+	e--
+	fmt.Println(e)
+
+	var positiveNumber = +20
+	fmt.Println(positiveNumber)
+
+	negativeNumber := -positiveNumber
+	fmt.Println(negativeNumber)
+
+	var isLoading = true
+	fmt.Println(isLoading)
+
+	varIsNotLoading := !isLoading
+	fmt.Println(varIsNotLoading)
 }
